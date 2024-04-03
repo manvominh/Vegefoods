3ff3293326ea4818760d353ca3d1d6387fc223e3
manvominh <vominhman@gmail.com>
2024-04-03 16:48:34 +0700

    add  Observable<Product[]>

--- vegefoods.angularui/src/app/Services/product.service.ts
+++ vegefoods.angularui/src/app/Services/product.service.ts
@@ -1,7 +1,8 @@
-import { Product } from './../Pages/products/product';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
+import { Product } from './../Pages/products/product';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class ProductService {
   private baseUrl:string = environment.baseUrl;
   constructor(private httpClient: HttpClient) { }
 
-  getProducts(){
+  getProducts(): Observable<Product[]> {
     return this.httpClient.get<Product[]>(`${this.baseUrl}/Products`);
   }
 }
