7eb21b1faec1fce1f20cc77521d053249157bf1f
manvominh <vominhman@gmail.com>
2024-04-25 16:20:55 +0700

    fixed errors The field is required + change baseurl in development mode.

--- Vegefoods.Application/Dtos/UserProfileDto.cs
+++ Vegefoods.Application/Dtos/UserProfileDto.cs
@@ -7,12 +7,12 @@ namespace Vegefoods.Application.Dtos
 	public class UserProfileDto : IMapFrom<User>
 	{
 		public int Id { get; set; }
-		public string FirstName { get; set; }
-		public string LastName { get; set; }
-		public string Gender { get; set; }
+		public string? FirstName { get; set; }
+		public string? LastName { get; set; }
+		public string? Gender { get; set; }
 		public DateTime? DateOfBirth { get; set; }
 		public string? Address { get; set; }
-		public string Country { get; set; }
-		public string Phone { get; set; }
+		public string? Country { get; set; }
+		public string? Phone { get; set; }
 	}
 }
--- vegefoods.angularui/src/environments/environment.development.ts
+++ vegefoods.angularui/src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production:true,
-    baseUrl:'https://localhost:7090/api'
+    baseUrl:'http://localhost:5296/api'
 };
